b327e99a946b2a8c39b99dd3f10853171f804218
Quaqzilla <bbrigett207@gmail.com>
2025-07-02 13:04:04 +0200

    Putting the website live

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css';
-import {BrowserRouter, Routes, Route} from 'react-router';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Landing from './pages/Landing/Landing';
 
 const App = () => {
   return(
-      <BrowserRouter>
+      <BrowserRouter basename='/Mokotedi-Enterprises'>
         <Routes>
           <Route path='/' element={<Landing/>}/>
 
