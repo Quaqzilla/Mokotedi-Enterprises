082c1c59ea72c62cafd30d862892433cbd055568
Quaqzilla <bbrigett207@gmail.com>
2025-07-02 22:12:45 +0200

    about us page and navigation

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Landing from './pages/Landing/Landing';
+import About from './pages/About/About';
 
 const App = () => {
   return(
       <BrowserRouter basename='/Mokotedi-Enterprises'>
         <Routes>
           <Route path='/' element={<Landing/>}/>
+          <Route path='/About-Us' element={<About/>}/>
         </Routes>
       </BrowserRouter>
   )
