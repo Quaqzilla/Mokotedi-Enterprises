2b28ac5d1a1aecb8c89082b834e3a7994f34ce30
Quaqzilla <bbrigett207@gmail.com>
2025-07-08 00:54:49 +0200

    beginning os Services section

--- Frontend/src/pages/About/About.jsx
+++ Frontend/src/pages/About/About.jsx
@@ -1,9 +1,15 @@
 import './About.css';
+import { useEffect } from 'react';
 import NavBar from '../../components/NavBar/NavBar';
 import AboutSection from '../../components/AboutSection/AboutSection';
 import Footer from '../../components/Footer/Footer';
 
 const About = () => {
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
+
     return(
         <div>
             <NavBar/>
