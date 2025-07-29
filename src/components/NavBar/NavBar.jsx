b7d2396d6983720936f073791f5e6f5dcf998e81
Quaqzilla <bbrigett207@gmail.com>
2025-07-29 23:40:19 +0200

    .

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -70,7 +70,7 @@ const NavBar = () => {
                 <ul className={menuOpen ? 'open' : ''}>
                     <a href="" data-aos = "zoom-in" data-aos-duration = "2000" onClick={AboutUs}>About Us</a>
                     <a href="" data-aos = "zoom-in" data-aos-duration = "2200" onClick={Services}>Services</a>
-                    <a href="" data-aos = "zoom-in" data-aos-duration = "2400">Projects</a>
+                    <a href="" data-aos = "zoom-in" data-aos-duration = "2400" onClick={Work}>Projects</a>
                 </ul>
             </div>
 
